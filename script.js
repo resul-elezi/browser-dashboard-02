c28f14a7bc4de23f6e3caddb66326d8d4ab32fb2
Resul Elezi <resul_elezi@protonmail.com>
2025-11-03 15:48:09 +0100

    #

--- script.js
+++ script.js
@@ -204,8 +204,8 @@ window.addEventListener('load', () => {
 
 // ** Performance visualization
 // Chart with canvas
-const xLine = [10, 20, 30, 40, 50, 60];
-const yLine = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const xLine = [0, 1, 2, 3, 4, 5, 6];
+const yLine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const now = new Date();
 log(now.getMinutes());
