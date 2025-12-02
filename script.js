ce25f6c475831a374a3a9cff4447c1a3a28304e0
Resul Elezi <resul_elezi@protonmail.com>
2025-12-02 08:47:03 +0100

    #

--- script.js
+++ script.js
@@ -423,7 +423,7 @@ const showPrompt = byId('show-prompt');
       </section> */
 
 const dashboardContent = $('.dashboard');
-const overlay = byId('overlay');
+const overlay = $('.overlay');
 const modalTitle = 'Alert ';
 const contentForModals = `<section class="panel modal" id="">
         <div class="panel-header">
